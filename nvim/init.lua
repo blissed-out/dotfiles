56e3be60151e8780fdc1d50ebc2c00b64ea81493
blissed-out <aadarshbanjade.computer@gmail.com>
2025-04-29 07:37:32 +0545

    chores: update as plugins change

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,25 +1,2 @@
 require("blissful.core")
 require("blissful.lazy")
--- if vim.g.neovide then
--- 	vim.o.guifont = "IBM Plex Mono:h18"
---
--- 	vim.opt.linespace = 35
--- 	vim.g.neovide_scale_factor = 1.0
--- 	local change_scale_factor = function(delta)
--- 		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
--- 	end
--- 	vim.keymap.set("n", "<C-=>", function()
--- 		change_scale_factor(1.25)
--- 	end)
--- 	vim.keymap.set("n", "<C-->", function()
--- 		change_scale_factor(1 / 1.25)
--- 	end)
--- end
-
--- vim.api.nvim_create_autocmd("TermOpen", {
--- 	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
--- 	callback = function()
--- 		vim.opt.number = false
--- 		vim.opt.relativenumber = false
--- 	end,
--- })
