eb3aa154f5b01151f135668b4a647e6af356189b
blissed-out <aadarshbanjade.computer@gmail.com>
2025-04-29 07:34:30 +0545

    feat: replace rose pine with catppuccin

--- nvim/lua/blissful/plugins/colorscheme.lua
+++ nvim/lua/blissful/plugins/colorscheme.lua
@@ -1,25 +1,174 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	-- "sainnhe/gruvbox-material",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	-- Optionally configure and load the colorscheme
+	-- 	-- directly inside the plugin declaration.
+	-- 	vim.g.gruvbox_material_enable_italic = true
+	-- 	vim.g.gruvbox_material_background = "medium"
+	-- 	vim.g.gruvbox_material_transparent_background = 0
+	-- 	vim.g.gruvbox_material_foreground = "material"
+	-- 	vim.g.gruvbox_material_visual = "green"
+	-- 	vim.g.gruvbox_material_menu_selection_background = "green"
+	-- 	vim.g.gruvbox_material_show_eob = 0
+	-- 	vim.g.gruvbox_material_float_style = "dim"
+	--
+	-- 	vim.cmd.colorscheme("gruvbox-material")
+	--
+	-- 	vim.api.nvim_set_hl(0, "Pmenu", { bg = "#1d2021" }) -- Darker gray (or blackish tone)
+	-- 	-- vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#3c3836", blend = 0 }) -- Slightly lighter for selected item
+	-- 	-- vim.api.nvim_set_hl(0, "PmenuSbar", { bg = "#1d2021" })
+	-- 	-- vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#1d2021" })
+	--
+	-- 	-- Ensure floating windows (e.g., completion, popups) are darker
+	-- 	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1d2021" }) -- Dark background for floats
+	-- 	-- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#1d2021" })
+	-- end,
+	--
+	-- "AlexvZyl/nordic.nvim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	require("nordic").setup({})
+	-- 	vim.cmd.colorscheme("nordic")
+	-- end,
+	-- --
+	-- "neanias/everforest-nvim",
+	-- -- Optional; default configuration will be used if setup isn't called.
+	-- config = function()
+	-- 	require("everforest").setup({
+	-- 		-- background = "hard",
+	-- 		-- transparent_background_level = 2,
+	-- 		-- italics = true,
+	-- 		-- disable_italic_comments = false,
+	-- 		-- sign_column_background = "none",
+	-- 		-- ui_contrast = "low",
+	-- 		-- dim_inactive_windows = false,
+	-- 		-- diagnostic_text_highlight = true,
+	-- 		-- diagnostic_virtual_text = "coloured",
+	-- 		-- spell_foreground = false,
+	-- 	})
+	-- 	vim.cmd.colorscheme("everforest")
+	-- end,
+	-- "ramojus/mellifluous.nvim",
+	-- -- version = "v0.*", -- uncomment for stable config (some features might be missed if/when v1 comes out)
+	-- config = function()
+	-- 	require("mellifluous").setup({
+	-- 		mellifluous = {
+	-- 			neutral = false, -- set this to false for original mellifluous (when it was called meliora theme)
+	-- 		},
+	-- 	}) -- optional, see configuration section.
+	-- 	vim.cmd.colorscheme("mellifluous")
+	-- end,
+	--
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
 	config = function()
-		require("rose-pine").setup({
-			variant = "auto", -- auto, main, moon, or dawn
-			dark_variant = "main", -- main, moon, or dawn
-			-- dim_inactive_windows = false,
-			-- extend_background_behind_borders = true,
-			--
-			styles = {
-				bold = true,
-				italic = false,
-				transparency = false,
+		require("catppuccin").setup({
+			flavour = "auto", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "frappe",
+				dark = "macchiato",
 			},
-
-			highlight_groups = {
-				Comments = { italic = true },
+			transparent_background = false, -- disables setting the background color.
+			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+			dim_inactive = {
+				enabled = false, -- dims the background color of inactive window
+				shade = "dark",
+				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+			},
+			no_italic = false, -- Force no italic
+			no_bold = false, -- Force no bold
+			no_underline = false, -- Force no underline
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				comments = { "italic" }, -- Change the style of comments
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = { "bold" },
+				-- miscs = {}, -- Uncomment to turn off hard-coded styles
+			},
+			color_overrides = {},
+			custom_highlights = function(colors)
+				return {
+					Pmenu = { bg = colors.none },
+				}
+			end,
+			default_integrations = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = false,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
 			},
 		})
-		vim.cmd("colorscheme rose-pine")
+		-- setup must be called before loading
+		vim.cmd.colorscheme("catppuccin")
 	end,
+
+	-- "rose-pine/neovim",
+	-- name = "rose-pine",
+	-- config = function()
+	-- 	require("rose-pine").setup({
+	-- 		variant = "auto", -- auto, main, moon, or dawn
+	-- 		dark_variant = "main", -- main, moon, or dawn
+	-- 		-- dim_inactive_windows = false,
+	-- 		-- extend_background_behind_borders = true,
+	--
+	-- 		styles = {
+	-- 			bold = true,
+	-- 			italic = false,
+	-- 			transparency = false,
+	-- 		},
+	--
+	-- 		highlight_groups = {
+	-- 			Comments = { italic = true },
+	-- 			operator = { italic = true },
+	-- 			conditional = { italic = true },
+	-- 		},
+	-- 	})
+	--
+	-- 	vim.cmd("colorscheme rose-pine")
+	-- end,
+	--
+	-- "craftzdog/solarized-osaka.nvim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- opts = {},
+	-- config = function()
+	-- 	vim.cmd.colorscheme("solarized-osaka")
+	-- end,
+
+	-- "rebelot/kanagawa.nvim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	vim.cmd.colorscheme("kanagawa")
+	-- end,
+
+	-- "folke/tokyonight.nvim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- opts = {},
+	-- config = function()
+	-- 	vim.cmd.colorscheme("tokyonight-night")
+	-- end,
 
 	-- "oxfist/night-owl.nvim",
 	-- lazy = false, -- make sure we load this during startup if it is your main colorscheme
@@ -41,7 +190,7 @@ return {
 	-- priority = 1000,
 	--
 	-- config = function()
-	-- 	vim.g.nord_contrast = false
+	-- 	vim.g.nord_contrast = true
 	-- 	vim.g.nord_borders = false
 	-- 	vim.g.nord_disable_background = false
 	-- 	vim.g.nord_italic = false
