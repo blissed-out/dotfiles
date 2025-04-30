f36d3236292bd603014ca5fa736bcd4d0f7dbe21
blissed-out <aadarshbanjade.computer@gmail.com>
2025-04-30 16:36:42 +0545

    feat: bufferline (for tabs)

--- nvim/lua/blissful/plugins/bufferline.lua
+++ nvim/lua/blissful/plugins/bufferline.lua
@@ -2,10 +2,10 @@ return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
-	-- opts = {
-	-- 	options = {
-	-- 		mode = "tabs",
-	-- 		separator_style = "slant",
-	-- 	},
-	-- },
+	opts = {
+		options = {
+			mode = "tabs",
+			separator_style = "thin",
+		},
+	},
 }
